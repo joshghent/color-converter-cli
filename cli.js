98a83063cd76fd4331cdb61aa62886917130d943
PhilippeChab <philippe.chab@hotmail.com>
2018-10-02 22:42:49 -0400

    More doc along with the readme

--- cli.js
+++ cli.js
@@ -7,9 +7,9 @@ vorpal
 
 vorpal
     .command('color-convert [color]')
-    .option('-h, --hex')
-    .option('-r, --rgb')
-    .option('-c, --cmyk')
+    .option('-h, --hex', 'Converts the color passed to Hexidecimal')
+    .option('-r, --rgb', 'Converts the color passed to RGB')
+    .option('-c, --cmyk', 'Converts the color passed to CMYK')
     .action(function (args, cb) {
         this.log(Color(args.color));
         this.log(args);
